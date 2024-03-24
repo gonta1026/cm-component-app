586755f1bd0d010ec6f6c10a4dd2d64a1c3ba5fb
gonta1026 <katano.original@gmail.com>
2024-03-25 07:37:34 +0900

    update: next version 14.1.4

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    optimizePackageImports: ['src/components/ui-parts/Sample', '@/components/ui-parts/Sample/HogeHoge'],
+  },
+};
 
 module.exports = nextConfig;
