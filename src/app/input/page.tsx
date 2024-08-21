1aa40c32be629d1557204f2f1587dd0fa53ef446
gonta1026 <katano.original@gmail.com>
2024-08-21 12:39:10 +0900

    fix

--- src/app/input/page.tsx
+++ src/app/input/page.tsx
@@ -43,74 +43,76 @@ export default function InputPage() {
     <div className={styles.input}>
       <CmContainer>
         <h2 className={styles.title}>Input</h2>
-        <div className={styles.content}>
-          <div className={styles.topBlock}>
-            <Link href={'/'} className={styles.prevLink}>
-              ← TopPage
-            </Link>
-            <div className={styles.showLink}>
-              Check it on{' '}
-              <a
-                target="_blank"
-                rel="noopener noreferrer"
-                className={styles.link}
-                href={'https://github.com/gonta1026/cm-component-app/tree/main/src/app/input'}
-              >
-                sample page
-              </a>
-            </div>
-            {COMPONENTS.map((component) => (
-              <div key={component.href} className={styles.showLink}>
-                <a target="_blank" rel="noopener noreferrer" className={styles.link} href={component.href}>
-                  - {component.name}
+        <form>
+          <div className={styles.content}>
+            <div className={styles.topBlock}>
+              <Link href={'/'} className={styles.prevLink}>
+                ← TopPage
+              </Link>
+              <div className={styles.showLink}>
+                Check it on{' '}
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className={styles.link}
+                  href={'https://github.com/gonta1026/cm-component-app/tree/main/src/app/input'}
+                >
+                  sample page
                 </a>
               </div>
-            ))}
+              {COMPONENTS.map((component) => (
+                <div key={component.href} className={styles.showLink}>
+                  <a target="_blank" rel="noopener noreferrer" className={styles.link} href={component.href}>
+                    - {component.name}
+                  </a>
+                </div>
+              ))}
+            </div>
+            <CmCard className={styles.card}>
+              <div className='testtest'>
+                <CmInput id="input2" />
+              </div>
+              <div>
+                <CmLabel htmlFor="input">normal</CmLabel>
+                <CmInput id="input" />
+                <CmHelpterText>hogehogehoge</CmHelpterText>
+              </div>
+              <div>
+                <CmLabel htmlFor="required" isRequired>
+                  normal
+                </CmLabel>
+                <CmInput id="required" />
+              </div>
+              <div>
+                <CmLabel htmlFor="error">error</CmLabel>
+                <CmInput id="error" isError />
+                <CmErrorText>fugafugafuga</CmErrorText>
+              </div>
+              <div>
+                <CmLabel htmlFor="disabled">disabled</CmLabel>
+                <CmInput id="disabled" isDisabled />
+              </div>
+              <div>
+                <CmLabel htmlFor="password">password</CmLabel>
+                <CmPasswordInput id="password" />
+              </div>
+              <div>
+                <CmLabel htmlFor="left-icon">left icon</CmLabel>
+                <CmIconInput
+                  id="left-icon"
+                  leftIcon={<Image src={search.src} alt={search.alt} width={18} height={18} />}
+                />
+              </div>
+              <div>
+                <CmLabel htmlFor="right-icon">right icon</CmLabel>
+                <CmIconInput
+                  id="right-icon"
+                  rightIcon={<Image src={search.src} alt={search.alt} width={18} height={18} />}
+                />
+              </div>
+            </CmCard>
           </div>
-          <CmCard className={styles.card}>
-            <div className='testtest'>
-              <CmInput id="input2" />
-            </div>
-            <div>
-              <CmLabel htmlFor="input">normal</CmLabel>
-              <CmInput id="input" />
-              <CmHelpterText>hogehogehoge</CmHelpterText>
-            </div>
-            <div>
-              <CmLabel htmlFor="required" isRequired>
-                normal
-              </CmLabel>
-              <CmInput id="required" />
-            </div>
-            <div>
-              <CmLabel htmlFor="error">error</CmLabel>
-              <CmInput id="error" isError />
-              <CmErrorText>fugafugafuga</CmErrorText>
-            </div>
-            <div>
-              <CmLabel htmlFor="disabled">disabled</CmLabel>
-              <CmInput id="disabled" isDisabled />
-            </div>
-            <div>
-              <CmLabel htmlFor="password">password</CmLabel>
-              <CmPasswordInput id="password" />
-            </div>
-            <div>
-              <CmLabel htmlFor="left-icon">left icon</CmLabel>
-              <CmIconInput
-                id="left-icon"
-                leftIcon={<Image src={search.src} alt={search.alt} width={18} height={18} />}
-              />
-            </div>
-            <div>
-              <CmLabel htmlFor="right-icon">right icon</CmLabel>
-              <CmIconInput
-                id="right-icon"
-                rightIcon={<Image src={search.src} alt={search.alt} width={18} height={18} />}
-              />
-            </div>
-          </CmCard>
-        </div>
+        </form>
         <CmSpinner />
       </CmContainer>
     </div>
